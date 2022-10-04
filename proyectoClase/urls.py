8dd72c3f6623e8dd0da0682f22a9c9a9c1633ed5
Cristian D Biancotti <cdbiancotti@gmail.com>
2022-10-03 21:10:58 -0300

    Clase 19 - Playground Intermedio (parte 1)

--- proyectoClase/urls.py
+++ proyectoClase/urls.py
@@ -14,20 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-# from .views import hola, fecha
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('hola/', views.hola),
-    path('fecha/', views.fecha),
-    path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
-    path('mi-template/', views.mi_template),
-    path('mi-template/<str:nombre>', views.tu_template),
-    path('prueba-template/', views.prueba_template),
-    path('ver-personas/', views.ver_personas),
-    path('crear-persona/<str:nombre>/<str:apellido>/', views.crear_persona),
-    # path('hola/', hola),
-    # path('fecha/', fecha),
+    # path('home/', include('home.urls')),
+    path('', include('home.urls')),
+    # path('', include('otraAplicacion.urls')),
     path('admin/', admin.site.urls),
 ]
